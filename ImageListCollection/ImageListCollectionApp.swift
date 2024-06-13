7050b5d49cdd3c75ad52686dc6d29fbe700641ea
altek.global <altek.globals@gmail.com>
2024-06-13 10:12:28 +0700

    Initial Commit

---
+++ ImageListCollection/ImageListCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageListCollectionApp.swift
+//  ImageListCollection
+//
+//  Created by Khoa Nguyen on 13/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageListCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
